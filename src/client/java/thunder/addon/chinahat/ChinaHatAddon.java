40abe1296e9e99f88ec5f85b45dd93c4f622187c
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-07-11 18:54:26 +0300

    1

---
+++ src/client/java/thunder/addon/chinahat/ChinaHatAddon.java
@@ -0,0 +1,56 @@
+package thunder.addon.chinahat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import thunder.addon.chinahat.modules.ChinaHat;
+import thunder.hack.api.IAddon;
+import thunder.hack.cmd.Command;
+import thunder.hack.gui.hud.HudElement;
+import thunder.hack.modules.Module;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ChinaHatAddon implements IAddon {
+    public static final Logger LOGGER = LoggerFactory.getLogger("thchinahat");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Injecting china..");
+    }
+
+    @Override
+    public List<Module> getModules() {
+        return List.of(new ChinaHat());
+    }
+
+    @Override
+    public List<Command> getCommands() {
+        return null;
+    }
+
+    @Override
+    public List<HudElement> getHudElements() {
+        return null;
+    }
+
+    @Override
+    public String getPackage() {
+        return "thunder.addon.chinahat";
+    }
+
+    @Override
+    public String getName() {
+        return "ChinaHat";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Pan4ur";
+    }
+
+    @Override
+    public String getRepo() {
+        return "https://github.com/Pan4ur/ChinaHat-Addon";
+    }
+}
